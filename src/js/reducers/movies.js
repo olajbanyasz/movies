01918503fa5842ee88ce4b472b8402195b91ecaf
André Zsolt <andrezsolt71@gmail.com>
2019-02-06 20:42:19 +0100

    review fix part 1

--- src/js/reducers/movies.js
+++ src/js/reducers/movies.js
@@ -1,53 +1,46 @@
-import {  LOAD_MOVIES,
-          LOAD_MOVIES_SUCCESS,
-          LOAD_MOVIES_FAILED,
-          LOAD_ONE_MOVIE,
-          LOAD_ONE_MOVIE_SUCCESS,
-          LOAD_ONE_MOVIE_FAILED,
-          SELECT_ONE_MOVIE
-       } from '../actions/actionTypes';
+import actionTypes from '../actions/actionTypes'
 import defaultState from '../default-state';
 
 function movieReducer(state = defaultState.movies, action) {
   switch (action.type) {
-    case 'RESET_STORE':
+    case actionTypes.RESET_STORE:
       return defaultState.movies;
-    case 'LOAD_ONE_MOVIE':
+    case actionTypes.LOAD_ONE_MOVIE:
       return {
         ...state,
         selectedMovie: action.movieId,
         selectedMovieStatus: 'LOADING'
       };
-    case 'LOAD_ONE_MOVIE_SUCCESS':
+    case actionTypes.LOAD_ONE_MOVIE_SUCCESS:
       state.data.push(action.movie);
       return {
         ...state,
         data: _.uniqBy(state.data, 'id'),
         selectedMovieStatus: 'LOAD_ONE_MOVIE_SUCCESS'
       };
-    case 'LOAD_ONE_MOVIE_FAILED':
+    case actionTypes.LOAD_ONE_MOVIE_FAILED:
       return {
         ...state,
         selectedMovieStatus: 'LOAD_ONE_MOVIE_FAILED'
       };
-    case 'LOAD_MOVIES':
+    case actionTypes.LOAD_MOVIES:
       return {
         ...state,
         status: 'LOADING',
       };
-    case 'LOAD_MOVIES_SUCCESS':
+    case actionTypes.LOAD_MOVIES_SUCCESS:
       return {
         ...state,
         data: action.movies,
         status: 'LOAD_MOVIES_SUCCESS',
       };
-    case 'LOAD_MOVIES_FAILED':
+    case actionTypes.LOAD_MOVIES_FAILED:
       return {
         ...state,
         data: [],
         status: 'LOAD_MOVIES_FAILED',
       };
-    case 'SELECT_ONE_MOVIE':
+    case actionTypes.SELECT_ONE_MOVIE:
       return {
         ...state,
         selectedMovie: action.movieId,
